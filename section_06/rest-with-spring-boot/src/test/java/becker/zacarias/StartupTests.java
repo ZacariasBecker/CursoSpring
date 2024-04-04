0732aa9fcbbe82ea130885e3be0da8e4b6eec0a8
ZacariasBecker <zacariasbecker@gmail.com>
2024-04-04 11:45:18 -0300

    section 5

---
+++ section_06/rest-with-spring-boot/src/test/java/becker/zacarias/StartupTests.java
@@ -0,0 +1,13 @@
+package becker.zacarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
